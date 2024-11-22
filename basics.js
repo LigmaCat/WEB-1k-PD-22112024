101859359ea9207c4dcd271976dfa50f7260fbcd
Edgars <johnCena@gmail.com>
2024-11-22 10:19:19 +0200

    first commit

---
+++ basics.js
@@ -0,0 +1,30 @@
+// personAge  uzd
+
+// let personAge = 17
+
+// if (personAge == 17) {
+// console.log("Nepilgadīgs")
+// }else{
+//     console.log("Pilgadīgs")
+// }
+
+// console.log(personAge)
+
+// Array uzd
+
+let namesArray = ["Jānis", "Anna", "Zaiga"]
+
+// console.log(namesArray[2])
+
+
+
+for (let i = 0; i <= 2; i++) {
+    console.log(namesArray[i])
+}
+
+function sayHello() {
+    
+    console.log(sayHello)
+    }
+  
+
